12b291f5439d15c591877405eeba90941607f8ae
Botche <gabrielpetkov622@gmail.com>
2020-04-08 13:21:08 +0300

    Add Gallery page to restaurants. Add back to previous page buttons to some pages.

---
+++ RestaurantsPlatform/Web/RestaurantsPlatform.Web.ViewModels/Images/AllImagesFromRestaurantViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RestaurantsPlatform.Web.ViewModels.Images
+{
+    using System.Collections.Generic;
+
+    using RestaurantsPlatform.Data.Models.Restaurants;
+    using RestaurantsPlatform.Services.Mapping;
+
+    public class AllImagesFromRestaurantViewModel : IMapFrom<Restaurant>, IMapFrom<Category>
+    {
+        public IEnumerable<DetailsRestaurantImageViewModel> Images { get; set; }
+
+        public int Id { get; set; }
+
+        public string RestaurantName { get; set; }
+    }
+}
--- RestaurantsPlatform/Web/RestaurantsPlatform.Web/Controllers/RestaurantImagesController.cs
+++ RestaurantsPlatform/Web/RestaurantsPlatform.Web/Controllers/RestaurantImagesController.cs
@@ -6,24 +6,28 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using RestaurantsPlatform.Services.Data.Interfaces;
+    using RestaurantsPlatform.Web.ViewModels.Images;
     using RestaurantsPlatform.Web.ViewModels.Restaurants;
 
     using static RestaurantsPlatform.Common.GlobalConstants;
 
-    [Authorize(Roles = AdministratorOrRestaurantOwner)]
-    public class ImageRestaurantController : BaseController
+    public class RestaurantImagesController : BaseController
     {
         private readonly IRestaurantImageService imageService;
         private readonly IUserService userService;
+        private readonly IRestaurantService restaurantService;
 
-        public ImageRestaurantController(
+        public RestaurantImagesController(
             IRestaurantImageService imageService,
-            IUserService userService)
+            IUserService userService,
+            IRestaurantService restaurantService)
         {
             this.imageService = imageService;
             this.userService = userService;
+            this.restaurantService = restaurantService;
         }
 
+        [Authorize(Roles = AdministratorOrRestaurantOwner)]
         [HttpPost]
         public async Task<IActionResult> AddImageToRestaurant(AddPictureToRestaurantInputModel input)
         {
@@ -37,6 +41,18 @@
             await this.imageService.AddImageToRestaurantAsync(input.ImageUrl, input.RestaurantName, input.Id);
 
             return this.RedirectToRoute("restaurant", new { id = input.Id, name = input.RestaurantName });
+        }
+
+        public IActionResult Gallery(int id)
+        {
+            var restaurantImages = this.restaurantService.GetById<AllImagesFromRestaurantViewModel>(id);
+
+            if (restaurantImages == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(restaurantImages);
         }
     }
 }
