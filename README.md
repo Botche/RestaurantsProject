d479e255dd8aaddd5509ccc86f2a725e9bbb3a83
Botche <gabrielpetkov622@gmail.com>
2020-04-18 03:12:41 +0300

    Update js function and add asp-append-version to script resources in views.

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ Project for my graduation at SoftUni.
 
 TODO:
 * Reviews with comments
-* Update comments
 * Display recent comments and most popular
-* Display most popular
 * Favourites restaurants
 * Account page
 * RECaptcha
--- RestaurantsPlatform/Web/RestaurantsPlatform.Web/wwwroot/js/functions/favouriteButtonInRestaurantView.js
+++ RestaurantsPlatform/Web/RestaurantsPlatform.Web/wwwroot/js/functions/favouriteButtonInRestaurantView.js
@@ -2,28 +2,28 @@
     const likeBtn = document.getElementsByClassName('likeBtn')[0];
     const icon = document.getElementsByClassName('heartIcon')[0];
 
+    likeBtn.style.cursor = 'pointer';
     icon.classList.add('far', 'text-danger');
 
-    likeBtn.addEventListener('mouseover', function () {
-        if (icon.classList.contains('far')) {
-            icon.classList.replace('far', 'fa');
-        } else {
-            icon.classList.replace('fa', 'far');
-            icon.classList.add();
-        }
-    });
-    likeBtn.addEventListener('mouseout', function () {
-        if (icon.classList.contains('far')) {
-            icon.classList.replace('far', 'fa');
-        } else {
-            icon.classList.replace('fa', 'far');
-        }
-    });
+    likeBtn.addEventListener('mouseover', liked);
+    likeBtn.addEventListener('mouseout', liked);
     likeBtn.addEventListener('click', function () {
         if (icon.classList.contains('far')) {
             icon.classList.replace('far', 'fa');
+            likeBtn.addEventListener('mouseover', liked);
+            likeBtn.addEventListener('mouseout', liked);
         } else {
-            icon.classList.replace('fa','far');
+            icon.classList.replace('fa', 'far');
+            likeBtn.addEventListener('mouseover', liked);
+            likeBtn.addEventListener('mouseout', liked);
         }
     });
+
+    function liked() {
+        if (icon.classList.contains('far')) {
+            icon.classList.replace('far', 'fa');
+        } else {
+            icon.classList.replace('fa', 'far');
+        }
+    }
 })();
