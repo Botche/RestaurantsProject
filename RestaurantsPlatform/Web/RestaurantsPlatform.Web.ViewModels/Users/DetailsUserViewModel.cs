486614e01fd302a48f43569657256c4eebaf71d4
Botche <gabrielpetkov622@gmail.com>
2020-04-22 08:45:48 +0300

    Deploy app to azure

--- RestaurantsPlatform/Web/RestaurantsPlatform.Web.ViewModels/Users/DetailsUserViewModel.cs
+++ RestaurantsPlatform/Web/RestaurantsPlatform.Web.ViewModels/Users/DetailsUserViewModel.cs
@@ -1,11 +1,15 @@
 ﻿namespace RestaurantsPlatform.Web.ViewModels.Users
 {
     using System;
+    using System.Linq;
+
+    using AutoMapper;
 
     using RestaurantsPlatform.Data.Models;
+    using RestaurantsPlatform.Data.Models.Restaurants;
     using RestaurantsPlatform.Services.Mapping;
 
-    public class DetailsUserViewModel : IMapFrom<ApplicationUser>
+    public class DetailsUserViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string Email { get; set; }
 
@@ -20,5 +24,14 @@
         public int CommentsCount { get; set; }
 
         public int FavouriteRestaurantsCount { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ApplicationUser, DetailsUserViewModel>()
+                .ForMember(
+                    dest => dest.VotesCount,
+                    ori => ori.MapFrom(from => from.Votes
+                        .Count(vote => vote.Type != VoteType.Neutral)));
+        }
     }
 }
